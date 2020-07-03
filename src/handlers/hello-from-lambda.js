b35cdaa55ee61fb358163fb7cb1a0e3a7329f75c
Adeel Qureshi <adeel.qureshi@earnest.com>
2020-07-02 21:27:37 -0700

    added api event trigger

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -5,8 +5,10 @@ exports.helloFromLambdaHandler = async () => {
     // If you change this message, you will need to change hello-from-lambda.test.js
     const message = 'Hello from Lambda!';
 
-    // All log statements are written to CloudWatch
-    console.info(`${message}`);
+    const response = {
+        statusCode: 200,
+        body: message
+    }
     
-    return message;
+    return response;
 }
